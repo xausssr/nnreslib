35c48bf6d718d56df261f6988488353944af9b62
Андрей Толстых <xausssr@gmail.com>
2020-11-09 13:31:41 +0300

    Add prediction method

--- test.py
+++ test.py
@@ -25,8 +25,8 @@ nn.fit_lm(
         x_valid=valid_data.values[:,:-5],
         y_valid=valid_data.values[:,-5:],
         mu_init=5.0, 
-        min_error=2.083e-4 , 
-        max_steps=2500, 
+        min_error=2.083e-4, 
+        max_steps=100, 
         mu_multiply=10, 
         mu_divide=10, 
         m_into_epoch=10, 
@@ -35,3 +35,6 @@ nn.fit_lm(
 
 # Отрисовка обучения ИНС
 nn.plot_lw(None, save=False)
+
+print(nn.predict(valid_data.values[:,:-5], raw=True)[:10])
+print(nn.predict(valid_data.values[:,:-5], raw=False)[:10])
