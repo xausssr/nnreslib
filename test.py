3eb93e310ffa8346d27bd3675dbec4cf6220d6f9
Андрей Толстых <xausssr@gmail.com>
2020-11-14 19:15:51 +0300

    Working prototype CNN see #4

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
 import pandas as pd
+import numpy as np
 from somlib import NeuralNet
 
 path = Path(__file__).parent / "data"
 
 print("\n\nТест сверточной сети на наборе MNIST")
+
+train_data = pd.read_csv(path / 'mnist_train.csv', delimiter=',').values[:500]
+valid_data = pd.read_csv(path / 'mnist_test.csv', delimiter=',').values[:500]
 
 architecture = {
     "cl1": {"type": "convolution", "filtres": 8, "kernel" : [3, 3], "stride": [2, 2], "pad": [0, 0], "activation" : "relu"},
@@ -20,7 +24,7 @@ architecture = {
 
 settings = {
     "outs": 10,
-    "batch_size": 120,
+    "batch_size": 100,
     "architecture": architecture,
     "inputs": [28,28,1],
     "activation": "sigmoid",
@@ -28,7 +32,28 @@ settings = {
 
 # Построение CНС
 nn = NeuralNet(settings, verbose=True)
-exit()
+
+nn.fit_lm(
+    x_train=train_data[:,1:].reshape((-1,28,28,1)),
+    y_train=np.eye(10)[train_data[:, 0]],
+    x_valid=valid_data[:,1:].reshape((-1,28,28,1)),
+    y_valid=np.eye(10)[valid_data[:, 0]],
+    mu_init=5.0,
+    min_error=2.083e-4,
+    max_steps=6,
+    mu_multiply=5,
+    mu_divide=5,
+    m_into_epoch=5,
+    verbose=True,
+)
+
+# Отрисовка обучения ИНС
+nn.plot_lw(None, save=False, logscale=False)
+
+print(nn.predict(valid_data.values[:, :-5], raw=True)[:10])
+print(nn.predict(valid_data.values[:, :-5], raw=False)[:10])
+
+print("\n\nТест полносвязной сети")
 
 train_data = pd.read_csv(path / "train.csv")
 valid_data = pd.read_csv(path / "test.csv")
