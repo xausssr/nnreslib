1d0ce5ff1dcbd0ca355cbac0b4f0198f8d0832c8
zudik <vutden1@yandex.ru>
2020-11-14 17:51:02 +0300

    Add type hinting and rename variables

--- utils/hyperparametres.py
+++ utils/hyperparametres.py
@@ -1,82 +1,82 @@
 import math
 import numpy as np
+from typing import List, Tuple
 
-
-def det_stop(Q, P, gamma=0.5):
+def det_stop(len_dataset: int, len_output: int, gamma: float = 0.5) -> Tuple[float, float]:
     """
     Детерминированный способ нахождения критерия останова обучения
-    Q : int - объем обучающей выборки
-    P : int - кол-во нейронов в выходном слое
+    len_dataset : int - объем обучающей выборки
+    len_output : int - кол-во нейронов в выходном слое
     gamma : float - порог ошибок решений
     
-    E_H_S_lg - критерий остановки в децибелах
+    e_h_s_lg - критерий остановки в децибелах
     """
-    E_N_0 = 0.25
-    E_N_S = 1/(2*Q*P)*np.max([gamma**2, (1-gamma)**2])
-    E_H_S = E_N_S/E_N_0
-    E_H_S_lg = -3-10*np.log10(Q)-10*np.log10(P)
-    return E_H_S, E_H_S_lg
+    e_n_0 = 0.25
+    e_n_s = 1 / (2 * len_dataset * len_output) * np.max([gamma ** 2, (1 - gamma) ** 2])
+    e_h_s = e_n_s / e_n_0
+    e_h_s_lg = - 3 - 10 * np.log10(len_dataset) - 10 * np.log10(len_output)
+    return e_h_s, e_h_s_lg
 
-def count_epochs(Q, P, M, E_H_S_lg, neurons):
+def count_epochs(len_dataset: int, len_output: int, len_input: int, e_h_s_lg: float, neurons: List[int]) -> int:
     """
     Вычисляет кол-во эпох для обучения
-    Q : int - объем обучающей выборки
-    P : int - кол-во нейронов в выходном слое
-    M : int - кол-во входных параметров
-    E_H_S_lg : float - критерий остановки в децибелах
+    len_dataset : int - объем обучающей выборки
+    len_output : int - кол-во нейронов в выходном слое
+    len_input : int - кол-во входных параметров
+    e_h_s_lg : float - критерий остановки в децибелах
     neurons : list, tuple - распределение нейронов по скрытым слоям   
     """
 
-    if E_H_S_lg > 0:
-        raise ValueError('E_H_S_lg must be negative')
+    if e_h_s_lg > 0:
+        raise ValueError('e_h_s_lg must be negative')
 
-    s = Q*np.sqrt(-0.5*E_H_S_lg)
-    W_E = (M + 1)*neurons[0] 
-    for i in range(len(neurons)-1):
-        W_E += (neurons[i] + 1)*neurons[i+1]
-    W_E += (neurons[-1] + 1)*P
-    W_MP = W_E/(M*P)
-    N_E = np.sum(neurons) + P + len(neurons) + 1
-    w_N = W_E/N_E
-    S = s/(W_MP*w_N)**(1/4)
-    S += S*0.1 
-    return math.ceil(S)
+    s_value = len_dataset * np.sqrt(-0.5 * e_h_s_lg)
+    w_e = (len_input + 1) * neurons[0] 
+    for i in range(len(neurons) - 1):
+        w_e += (neurons[i] + 1) * neurons[i+1]
+    w_e += (neurons[-1] + 1) * len_output
+    w_mp = w_e / (len_input * len_output)
+    n_e = np.sum(neurons) + len_output + len(neurons) + 1
+    w_n = w_e / n_e
+    s_value_all = s_value / (w_mp * w_n) ** (1 / 4)
+    s_value_all += s_value_all * 0.1 
+    return math.ceil(s_value_all)
 
-def num_neurons(m, p, q, h=2, border_assessment="up"):    
+def num_neurons(len_input: int, len_output: int, len_dataset: int, num_hidden_layers: int = 2, border_assessment: str = "up") -> List[int]:    
     """
-    m - кол-во входов НС(число входных параметров)
-    p - кол-во нейронов в выходном слое НС
-    q - объем обучающей выборки
-    h - количество скрытых слоев
+    len_input - кол-во входов НС(число входных параметров)
+    len_output - кол-во нейронов в выходном слое НС
+    len_dataset - объем обучающей выборки
+    num_hidden_layers - количество скрытых слоев
     border_assessment : "low", "mid", "up" 
     """
 
-    w_low = p*q/(1 + math.log2(q))  # w - синаптические веса
-    w_up = p*(q/m + 1)*(m + p + 1) + p
-    w_mid = (w_low + w_up)/2
+    w_low = len_output * len_dataset/(1 + math.log2(len_dataset))  # w - синаптические веса
+    w_up = len_output * (len_dataset/len_input + 1) * (len_input + len_output + 1) + len_output
+    w_mid = (w_low + w_up) / 2
 
     if border_assessment == "low":
-        n = w_low/(m + p)
+        n_value = w_low / (len_input + len_output)
     elif border_assessment == "mid":
-        n = w_mid/(m + p)
+        n_value = w_mid / (len_input + len_output)
     elif border_assessment == "up":
-        n = w_up/(m + p)
+        n_value = w_up / (len_input + len_output)
 
-    if h == 1:
-        return math.ceil(n)
-    if h == 2:
-        n1 = math.ceil(2/3*n + 1)
-        n2 = math.ceil(1/3*n + 1)
+    if num_hidden_layers == 1:
+        return math.ceil(n_value)
+    if num_hidden_layers == 2:
+        n1 = math.ceil(2 / 3 * n_value + 1)
+        n2 = math.ceil(1 / 3 * n_value + 1)
         return n1, n2
     
-def find_hyperparametres(Q, P, M, h=2):
-	stop = det_stop(Q, P)
-	neurons_low = num_neurons(M, P, Q, h, border_assessment='low')
-	neurons_mid = num_neurons(M, P, Q, h, border_assessment='mid')
-	neurons_up = num_neurons(M, P, Q, h, border_assessment='up')
-	epochs_low = count_epochs(Q, P, M, stop[1], neurons_low)
-	epochs_mid = count_epochs(Q, P, M, stop[1], neurons_mid)
-	epochs_up = count_epochs(Q, P, M, stop[1], neurons_up)
+def find_hyperparametres(len_dataset: int, len_output: int, len_input: int, num_hidden_layers: int = 2) -> dict:
+	stop = det_stop(len_dataset, len_output)
+	neurons_low = num_neurons(len_input, len_output, len_dataset, num_hidden_layers, border_assessment='low')
+	neurons_mid = num_neurons(len_input, len_output, len_dataset, num_hidden_layers, border_assessment='mid')
+	neurons_up = num_neurons(len_input, len_output, len_dataset, num_hidden_layers, border_assessment='up')
+	epochs_low = count_epochs(len_dataset, len_output, len_input, stop[1], neurons_low)
+	epochs_mid = count_epochs(len_dataset, len_output, len_input, stop[1], neurons_mid)
+	epochs_up = count_epochs(len_dataset, len_output, len_input, stop[1], neurons_up)
 
 	dict_hyperparam = {'stop':stop[0],
 		        'stop_dB':stop[1],
