e289b8aab3effe543bb4cb989a74f073defb06a1
Maxim Elchugin <melchugin@gmail.com>
2020-12-06 19:26:22 +0300

    Add extra requirements. Update main requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import re
-from typing import List
+from typing import Dict, List
 
 from setuptools import find_packages, setup
 
@@ -21,9 +21,22 @@ def get_requirements() -> List[str]:
     return ret
 
 
+def get_extra_requirements() -> Dict[str, List[str]]:
+    ret: Dict[str, List[str]] = {"simple": [], "interactive": []}
+    with open("requirements/extra.txt", encoding="utf-8") as input_fd:
+        line: str
+        for line in input_fd:
+            if line.startswith("matplotlib"):
+                ret["simple"].append(line.rstrip())
+            else:
+                ret["interactive"].append(line.rstrip())
+    return ret
+
+
 setup(
     version=get_version(),
     packages=find_packages(include=["nnreslib", "nnreslib.*"]),
     package_data={"nnreslib": ["py.typed"]},
     install_requires=get_requirements(),
+    extras_require=get_extra_requirements(),
 )
