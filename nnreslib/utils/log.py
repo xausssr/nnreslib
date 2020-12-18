1d644548589e7294cd6b77be1750d68be515101c
Maxim Elchugin <melchugin@gmail.com>
2020-12-19 00:22:11 +0300

    Add todo comment

--- nnreslib/utils/log.py
+++ nnreslib/utils/log.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+# TODO: think about configuration compatible with pytest
 logging.basicConfig(
     level=logging.INFO if os.environ.get("DEBUG", "0") == "0" else logging.DEBUG,
     format="[%(asctime)s.%(msecs)03d] %(name)-45s %(levelname)8s %(message)s",
