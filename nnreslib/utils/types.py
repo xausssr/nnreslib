812a1c780e5e8393753b26afff964e1e47b74324
Maxim Elchugin <melchugin@gmail.com>
2020-12-18 23:46:58 +0300

    Shape refactoring. * Add __init_- overloads * Support init from tuple/list * Implement __len__, __truediv__, __repr__ * Add caching in prod * Fix __eq__, __mul__ for other types

--- nnreslib/utils/types.py
+++ nnreslib/utils/types.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
+
+import collections.abc as ca
 import functools
 import operator
 from enum import Enum
-from typing import Generator
+from typing import TYPE_CHECKING, overload
 
 from .tf_helper import tf
+
+if TYPE_CHECKING:
+    from typing import Generator, Optional, Sequence, Tuple, Union
 
 
 class ActivationFunction(Enum):
@@ -14,19 +20,36 @@ class ActivationFunction(Enum):
 
 
 class Shape:
-    def __init__(self, *args: int, is_null: bool = False):
+    @overload
+    def __init__(self) -> None:
+        ...
+
+    @overload
+    def __init__(self, dim: Optional[Sequence[int]]) -> None:
+        ...
+
+    @overload
+    def __init__(self, dim: Optional[int], *dims: int, is_null: bool = False) -> None:
+        ...
+
+    # pylint:disable=keyword-arg-before-vararg
+    def __init__(self, dim: Optional[Union[Sequence[int], int]] = None, *dims: int, is_null: bool = False) -> None:
+        if dim is None:
+            self.dimension: Tuple[int, ...] = ()
+        elif isinstance(dim, ca.Sequence):
+            self.dimension = (*dim,)
+        else:
+            self.dimension = (dim, *dims)
+        self._prod = -1
         self.is_null = is_null
-        self.dimension = tuple([*args])
-        for dim in self.dimension:
-            if not isinstance(dim, int):
+        for dimension in self.dimension:
+            if not isinstance(dimension, int):
                 raise ValueError("Shape arguments must be int")
-            if not self.is_null and dim < 1:
+            if not self.is_null and dimension < 1:
                 raise ValueError("Empty or negative dimension")
 
-    def __mul__(self, mul: int) -> "Shape":
-        return type(self)(*(dim * mul for dim in self.dimension), is_null=self.is_null)
-
-    __rmul__ = __mul__
+    def __len__(self) -> int:
+        return len(self.dimension)
 
     def __iter__(self) -> Generator[int, None, None]:
         for dim in self.dimension:
@@ -35,19 +58,40 @@ class Shape:
     def __getitem__(self, index: int) -> int:
         return self.dimension[index]
 
-    @property
-    def prod(self) -> int:
-        if not self.dimension:
-            return 0
-        return functools.reduce(operator.mul, self.dimension, 1)
+    def __mul__(self, value: object) -> Shape:
+        if isinstance(value, int):
+            return type(self)(*(dim * value for dim in self.dimension), is_null=self.is_null)
+        raise TypeError(f"unsupported operand type(s) for *: '{type(self).__name__}' and '{type(value).__name__}'")
+
+    __rmul__ = __mul__
+
+    def __truediv__(self, value: object) -> Shape:
+        if isinstance(value, int):
+            return type(self)(*(dim // value for dim in self.dimension), is_null=self.is_null)
+        raise TypeError(f"unsupported operand type(s) for /: '{type(self).__name__}' and '{type(value).__name__}'")
 
     def __str__(self) -> str:
         dim_print = "x".join(str(dim) for dim in self.dimension)
         return f"Shape: {dim_print}"
 
-    __repr__ = __str__
+    def __repr__(self) -> str:
+        dim_print = ", ".join(str(dim) for dim in self.dimension)
+        if dim_print:
+            dim_print += ", "
+        dim_print += f"is_null={self.is_null}"
+        return f"Shape({dim_print})"
 
     def __eq__(self, shape: object) -> bool:
         if isinstance(shape, Shape):
             return self.dimension == shape.dimension
+        if isinstance(shape, (list, tuple)):
+            return self.dimension == shape
         return False
+
+    @property
+    def prod(self) -> int:
+        if not self.dimension:
+            return 0
+        if self._prod == -1:
+            self._prod = functools.reduce(operator.mul, self.dimension, 1)
+        return self._prod
--- tests/utils/test_types.py
+++ tests/utils/test_types.py
@@ -3,45 +3,86 @@ import pytest
 from nnreslib.utils.types import Shape
 
 
-def test_shape_from_list():
+def test_shape_from_none():
     with pytest.raises(ValueError, match=r".*int$"):
-        Shape([1, 2, 3])
+        Shape((1, None))
 
-
-def test_shape_from_tuple():
     with pytest.raises(ValueError, match=r".*int$"):
-        Shape((1, 2, 3))
+        Shape([None])
 
 
 def test_shape_with_zero():
-    with pytest.raises(ValueError, match=r".*negative*"):
+    with pytest.raises(ValueError, match=r".*negative.*"):
         Shape(1, 0, 3)
 
 
 def test_shape_with_negative_dimension():
-    with pytest.raises(ValueError, match=r".*negative*"):
+    with pytest.raises(ValueError, match=r".*negative.*"):
         Shape(2, -3)
 
 
+def test_shape_from_list():
+    assert Shape([]) == ()
+    assert Shape([1]) == (1,)
+    assert Shape([1, 2, 3]) == (1, 2, 3)
+    assert Shape([0, 0, 0], is_null=True) == (0, 0, 0)
+
+
+def test_shape_from_tuple():
+    assert Shape(()) == ()
+    assert Shape((1,)) == (1,)
+    assert Shape((1, 2, 3)) == (1, 2, 3)
+    assert Shape((0, 0, 0), is_null=True) == (0, 0, 0)
+
+
 def test_shape_init():
-    assert Shape().dimension == ()
-    assert Shape(1).dimension == (1,)
-    assert Shape(10, 20).dimension == (10, 20)
-    assert Shape(1, 2, 3, 4, 5).dimension == (1, 2, 3, 4, 5)
+    assert Shape(None) == ()
+    assert Shape() == ()
+    assert Shape(1) == (1,)
+    assert Shape(10, 20) == (10, 20)
+    assert Shape(1, 2, 3, 4, 5) == (1, 2, 3, 4, 5)
+    assert Shape(1, 0, is_null=True) == (1, 0)
+    assert Shape(0, 0, 0, is_null=True) == (0, 0, 0)
 
 
 def test_shape_mul():
-    assert Shape() == 2 * Shape()
-    assert Shape() == Shape() * 2
-    assert Shape(2, 4) == Shape(1, 2) * 2
-    assert Shape(2, 4) == 2 * Shape(1, 2)
-    assert Shape(4, 8) == 2 * Shape(1, 2) * 2
+    assert 2 * Shape() == Shape()
+    assert Shape() * 2 == Shape()
+    assert Shape(0, is_null=True) * 10 == (0,)
+    assert Shape(0, 0, 0, is_null=True) * 10 == (0, 0, 0)
+    assert Shape(1, 2) * 2 == Shape(2, 4)
+    assert 2 * Shape(1, 2) == Shape(2, 4)
+    assert 2 * Shape(1, 2) * 2 == Shape(4, 8)
+    with pytest.raises(TypeError, match=r".*unsupported.*str"):
+        Shape(4, 2) * "861"  # pylint:disable=expression-not-assigned
+    with pytest.raises(TypeError, match=r".*unsupported.*float"):
+        86.1 * Shape(4, 2)  # pylint:disable=expression-not-assigned
+    with pytest.raises(ValueError, match=r".*negative.*"):
+        Shape(4, 2) * -2  # pylint:disable=expression-not-assigned
+
+
+def test_shape_div():
+    assert Shape() / 2 == Shape()
+    with pytest.raises(ValueError, match=r".*negative.*"):
+        Shape(1) / 2  # pylint:disable=expression-not-assigned
+    assert Shape(0, is_null=True) / 10 == (0,)
+    assert Shape(0, 0, 0, is_null=True) / 10 == (0, 0, 0)
+    assert Shape(1, is_null=True) / 2 == Shape(0, is_null=True)
+    assert Shape(2, 4) / 2 == Shape(1, 2)
+    assert Shape(5, 10, 15) / 5 == Shape(1, 2, 3)
+    with pytest.raises(TypeError, match=r".*unsupported.*str"):
+        Shape(10, 8) / "861"  # pylint:disable=expression-not-assigned
+    with pytest.raises(TypeError, match=r".*unsupported.*float"):
+        Shape(10, 8) / 86.1  # pylint:disable=expression-not-assigned
 
 
 def test_shape_prod():
     assert Shape().prod == 0
     assert Shape(1, 2, 3).prod == 6
     assert Shape(1).prod == 1
+    shape = Shape(5, 6)
+    assert shape.prod == 30
+    assert shape.prod == 30
 
 
 def test_shape_iter():
@@ -60,8 +101,9 @@ def test_shape_index():
 def test_shape_equal():
     assert Shape() == Shape()
     assert Shape(1, 2, 3) == Shape(1, 2, 3)
+    assert Shape((1, 2, 3)) == (1, 2, 3)
+    assert Shape([1, 2, 3]) != [2, 3]
     assert Shape(1, 2, 3) != Shape(10)
-    assert Shape(1, 2) != Shape(10, 20)
     assert Shape(1) != 1
     assert Shape() != "test"
 
@@ -70,3 +112,9 @@ def test_shape_str():
     assert str(Shape()) == "Shape: "
     assert str(Shape(1)) == "Shape: 1"
     assert str(Shape(1, 2, 3)) == "Shape: 1x2x3"
+
+
+def test_shape_repr():
+    assert repr(Shape()) == "Shape(is_null=False)"
+    assert repr(Shape(1)) == "Shape(1, is_null=False)"
+    assert repr(Shape(1, 2, 3, is_null=True)) == "Shape(1, 2, 3, is_null=True)"
