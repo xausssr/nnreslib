Rewrite architecture definition:  * fully rewrite Model  * simplify calculation layer's shapes.  * fix layer's shapes dimension for default values  * move merge functions to separate module  * fix enum with functions  * remove Settings  * actualize layers tests  * add tests for Model  * annotate `Shape.__getitem__` for slice and add tests for it  * comment graph.py (for passing pre-commit, need rewrite for support new model architecture)

nnreslib/graph.py
@@ -1,68 +1,79 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, List, Tuple
+from typing import Any
 
-from .layers import ConvolutionLayer, FlattenLayer, FullyConnectedLayer, MaxPoolLayer
-from .utils.tf_helper import tf
+# from typing import List, Tuple
 
-if TYPE_CHECKING:
-    from .settings import Settings
+# from .layers import ConvolutionLayer, FlattenLayer, FullyConnectedLayer, MaxPoolLayer
+# from .utils.tf_helper import tf
 
 
 class ForwardGraph:
-    __slots__ = ("settings", "input_data", "input_labels", "parameters", "output", "vector_error", "train_loss")
+    __slots__ = (
+        "batch_size",
+        "model",
+        "input_data",
+        "input_labels",
+        "parameters",
+        "output",
+        "vector_error",
+        "train_loss",
+    )
 
     # TODO: what about dtype???
 
-    def __init__(self, settings: Settings) -> None:
-        self.settings = settings
+    # TODO: REWRITE!!!!!
 
-        self.input_data = tf.Variable(
-            name="input_data", dtype=tf.float64, shape=(self.settings.batch_size, *self.settings.inputs)
-        )
-        self.input_labels = tf.Variable(
-            name="input_labels", dtype=tf.float64, shape=(self.settings.batch_size, *self.settings.outputs)
-        )  # TODO: check name
+    def __init__(self, batch_size: int, model: Any) -> None:
+        self.batch_size = batch_size
+        self.model = model
 
-        self.parameters: List[Tuple[tf.Variable, tf.Variable]] = []
+        # self.input_data = tf.Variable(
+        #     name="input_data", dtype=tf.float64, shape=(self.settings.batch_size, *self.settings.inputs)
+        # )
+        # self.input_labels = tf.Variable(
+        #     name="input_labels", dtype=tf.float64, shape=(self.settings.batch_size, *self.settings.outputs)
+        # )  # TODO: check name
 
-        # Start building. TODO: may be need it log
-        current_node = self.input_data
-        for layer in self.settings.model.layers:
-            if isinstance(layer, ConvolutionLayer):
-                weights = tf.Variable(layer.weights)
-                biases = tf.Variable(layer.biases)
-                self.parameters.append((weights, biases))
-                pad = layer.pad / 2
-                current_node = layer.activation(
-                    tf.nn.conv2d(
-                        current_node,
-                        weights,
-                        (1, *layer.stride, 1),  # TODO: feature request
-                        padding=(0, 0, *pad, 0, 0),  # TODO: feature request
-                    )
-                    + biases
-                )
-            elif isinstance(layer, MaxPoolLayer):
-                current_node = tf.nn.max_pool(
-                    current_node,
-                    (1, *layer.kernel, 1),
-                    (1, *layer.stride, 1),
-                    padding="SAME",  # TODO: need fix???
-                )
-            elif isinstance(layer, FlattenLayer):
-                current_node = tf.reshape(current_node, (self.settings.batch_size, *layer.output_shape))
-            elif isinstance(layer, FullyConnectedLayer):
-                weights = tf.Variable(layer.weights)
-                biases = tf.Variable(layer.biases)
-                self.parameters.append((weights, biases))
-                current_node = layer.activation(tf.matmul(current_node, weights) + biases)
+        # self.parameters: List[Tuple[tf.Variable, tf.Variable]] = []
 
-        # tf.Varibale([w+b+w1+b1])
+        # # Start building. TODO: may be need log it
+        # current_node = self.input_data
+        # for layer in self.settings.model.layers:
+        #     if isinstance(layer, ConvolutionLayer):
+        #         weights = tf.Variable(layer.weights)
+        #         biases = tf.Variable(layer.biases)
+        #         self.parameters.append((weights, biases))
+        #         pad = layer.pad / 2
+        #         current_node = layer.activation(
+        #             tf.nn.conv2d(
+        #                 current_node,
+        #                 weights,
+        #                 (1, *layer.stride, 1),  # TODO: feature request
+        #                 padding=(0, 0, *pad, 0, 0),  # TODO: feature request
+        #             )
+        #             + biases
+        #         )
+        #     elif isinstance(layer, MaxPoolLayer):
+        #         current_node = tf.nn.max_pool(
+        #             current_node,
+        #             (1, *layer.kernel, 1),
+        #             (1, *layer.stride, 1),
+        #             padding="SAME",  # TODO: need fix???
+        #         )
+        #     elif isinstance(layer, FlattenLayer):
+        #         current_node = tf.reshape(current_node, (self.settings.batch_size, *layer.output_shape))
+        #     elif isinstance(layer, FullyConnectedLayer):
+        #         weights = tf.Variable(layer.weights)
+        #         biases = tf.Variable(layer.biases)
+        #         self.parameters.append((weights, biases))
+        #         current_node = layer.activation(tf.matmul(current_node, weights) + biases)
 
-        self.output = tf.squeeze(current_node)
-        self.vector_error = self.input_labels - self.settings.outputs
-        self.train_loss = tf.reduce_mean(tf.square(self.vector_error), name="Train_loss")
+        # # tf.Varibale([w+b+w1+b1])
+
+        # self.output = tf.squeeze(current_node)
+        # self.vector_error = self.input_labels - self.settings.outputs
+        # self.train_loss = tf.reduce_mean(tf.square(self.vector_error), name="Train_loss")
 
     def describe(self) -> None:
         ...

nnreslib/layers/input_layer.py
@@ -1,35 +1,21 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 from .base_layer import Layer
 
 if TYPE_CHECKING:
-    import numpy as np
-
     from ..utils.types import Shape
 
 
 class InputLayer(Layer):
-    def __init__(self, name: str, input_shape: Shape, input_data: Optional[np.ndarray] = None):
+    def __init__(self, name: str, input_shape: Shape):
         super().__init__(name)
         self._input_shape = input_shape
         self._output_shape = input_shape
-        self.input_data = input_data
-        self._input_mean: Optional[float] = None
-        self._input_std: Optional[float] = None
 
-    @property
-    def input_mean(self) -> float:
-        if self._input_mean is None:
-            raise NotImplementedError
-        return self._input_mean
+    def _set_output_shape(self) -> None:
+        self._output_shape = self._input_shape
 
-    @property
-    def input_std(self) -> float:
-        if self._input_std is None:
-            raise NotImplementedError
-        return self._input_std
-
-    def get_last_filters_count(self, last_filters_count: int = 0) -> int:
-        return self.output_shape[-1]
+    def set_shapes(self, main_input: Shape, *other_input: Shape) -> None:
+        return

nnreslib/layers/trainable_layer.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Callable, Optional, cast
+from abc import abstractmethod
+from typing import TYPE_CHECKING, Any, Optional
 
 from .base_layer import Layer
 from ..utils.initialization import Initialization
-from ..utils.types import MergeFunction
+from ..utils.merge import MergeInputs
 
 if TYPE_CHECKING:
     import numpy as np
@@ -18,18 +19,19 @@ class TrainableLayer(Layer):
         self,
         name: str,
         activation: ActivationFunction,
+        merge: Optional[MergeInputs] = None,
         initializer: Initialization = Initialization(),
-        merge_func: MergeFunction = MergeFunction.PASSTHROUGH,
         is_out: bool = False,
         **kwargs: Any
     ) -> None:
-        super().__init__(name, merge_func=merge_func, is_out=is_out, **kwargs)
+        super().__init__(name, merge=merge, is_out=is_out, **kwargs)
         self.initializer = initializer
-        self.activation: Callable = cast(Callable, activation)
+        self.activation = activation
         self._weights: Optional[np.ndarray] = None
         self._biases: Optional[np.ndarray] = None
 
     @property
+    @abstractmethod
     def weights_shape(self) -> Shape:
         ...
 
@@ -43,6 +45,7 @@ class TrainableLayer(Layer):
         self._weights = self.initializer.init_weights(self, data_mean, data_std)
 
     @property
+    @abstractmethod
     def biases_shape(self) -> Shape:
         ...
 
@@ -54,7 +57,3 @@ class TrainableLayer(Layer):
 
     def set_biases(self, data_mean: float = 0.0, data_std: float = 0.0) -> None:
         self._biases = self.initializer.init_biases(self, data_mean, data_std)
-
-    @property
-    def neurons_count(self) -> int:
-        ...

nnreslib/utils/initialization.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import collections.abc as ca
+import functools
 import math
 from enum import Enum
 from typing import TYPE_CHECKING, Callable
@@ -46,9 +47,9 @@ def _haykin(
 
 
 class StandartInitializer(Enum):
-    ZEROS = _zeros
-    HE_NORMAL = _he_normal
-    HAYKIN = _haykin
+    ZEROS = functools.partial(_zeros)
+    HE_NORMAL = functools.partial(_he_normal)
+    HAYKIN = functools.partial(_haykin)
 
 
 InitializerType = Callable[[Shape, Shape, Shape, float, float], np.ndarray]
@@ -56,7 +57,9 @@ InitializerType = Callable[[Shape, Shape, Shape, float, float], np.ndarray]
 
 class Initialization:
     def __init__(
-        self, weights_initializer: InitializerType = _zeros, biases_initializer: InitializerType = _he_normal
+        self,
+        weights_initializer: InitializerType = StandartInitializer.HE_NORMAL.value,
+        biases_initializer: InitializerType = StandartInitializer.ZEROS.value,
     ) -> None:
         if not all(
             map(lambda x: isinstance(x, ca.Callable), (weights_initializer, biases_initializer))  # type: ignore

nnreslib/utils/types.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import collections.abc as ca
 import functools
 import operator
-from enum import Enum
+from enum import Enum, unique
 from typing import TYPE_CHECKING, overload
 
 from .tf_helper import tf
@@ -11,27 +11,14 @@ from .tf_helper import tf
 if TYPE_CHECKING:
     from typing import Generator, Optional, Sequence, Tuple, Union
 
-    import numpy as np
 
-
+# TODO: support backend
+@unique
 class ActivationFunction(Enum):
-    SIGMOID = tf.nn.sigmoid
-    RELU = tf.nn.relu
-    TANH = tf.nn.tanh
-    SOFT_MAX = tf.nn.softmax
-
-
-def _passthroug(data: np.ndarray) -> np.ndarray:
-    return data
-
-
-def not_implemented() -> None:
-    raise NotImplementedError
-
-
-class MergeFunction(Enum):
-    PASSTHROUGH = _passthroug
-    ADD = not_implemented  # TODO: implement
+    SIGMOID = functools.partial(tf.nn.sigmoid)
+    RELU = functools.partial(tf.nn.relu)
+    TANH = functools.partial(tf.nn.tanh)
+    SOFT_MAX = functools.partial(tf.nn.softmax)
 
 
 class Shape:
@@ -40,7 +27,7 @@ class Shape:
         ...
 
     @overload
-    def __init__(self, dim: Optional[Sequence[int]]) -> None:
+    def __init__(self, dim: Optional[Sequence[int]], *, is_null: bool = False) -> None:
         ...
 
     @overload
@@ -70,8 +57,18 @@ class Shape:
         for dim in self.dimension:
             yield dim
 
+    @overload
     def __getitem__(self, index: int) -> int:
-        return self.dimension[index]
+        ...
+
+    @overload
+    def __getitem__(self, index: slice) -> Sequence[int]:
+        ...
+
+    def __getitem__(self, index: Union[int, slice]) -> Union[int, Sequence[int]]:
+        if isinstance(index, (int, slice)):
+            return self.dimension[index]
+        raise TypeError(f"index must be integer or slices, not {type(index).__name__}")
 
     def __mul__(self, value: object) -> Shape:
         if isinstance(value, int):