c312620e938561449f63c973eb9284035b8060ce
Maxim Elchugin <melchugin@gmail.com>
2021-01-13 01:10:27 +0300

    Merge branch 'big-refactoring' into fit-graph

--- nnreslib/backend/tf/graph.py
+++ nnreslib/backend/tf/graph.py
@@ -4,56 +4,56 @@ import tensorflow as tf
 
 from .. import DTYPE
 
-placeholder = partial(tf.compat.v1.placeholder, DTYPE.value)
-_placeholder = tf.compat.v1.placeholder
-variable = partial(tf.Variable, dtype=DTYPE.value)
-_variable = tf.Variable
-split = tf.split
+GradientDescentOptimizer = tf.compat.v1.train.GradientDescentOptimizer
+Operation = tf.Operation
+PlaceholderType = tf.compat.v1.placeholder
+Session = tf.compat.v1.Session
+Tensor = tf.Tensor
+VariableType = tf.Variable
+assign = tf.compat.v1.assign
 concat = tf.concat
 conv2d = tf.nn.conv2d
-max_pool = tf.nn.max_pool
-reshape = tf.reshape
-matmul = tf.matmul
-multiply = tf.multiply
-squeeze = tf.squeeze
-reduce_mean = tf.reduce_mean
-square = tf.square
-gradients = tf.gradients
-GradientDescentOptimizer = tf.compat.v1.train.GradientDescentOptimizer
 eye = partial(tf.eye, dtype=DTYPE.value)
-hessians = tf.hessians
-zeros = partial(tf.zeros, dtype=DTYPE.value)
-assign = tf.compat.v1.assign
-inv = tf.linalg.inv
-Session = tf.compat.v1.Session
 global_variables_initializer = tf.compat.v1.global_variables_initializer  # TODO: may be rename
-Tensor = tf.Tensor
-Operation = tf.Operation
+gradients = tf.gradients
+hessians = tf.hessians
+inv = tf.linalg.inv
+matmul = tf.matmul
+max_pool = tf.nn.max_pool
+multiply = tf.multiply
+placeholder = partial(tf.compat.v1.placeholder, DTYPE.value)
+reduce_mean = tf.reduce_mean
+reshape = tf.reshape
+split = tf.split
+square = tf.square
+squeeze = tf.squeeze
+variable = partial(tf.Variable, dtype=DTYPE.value)
+zeros = partial(tf.zeros, dtype=DTYPE.value)
 
 __all__ = [
-    "placeholder",
-    "_placeholder",
-    "variable",
-    "_variable",
-    "split",
+    "GradientDescentOptimizer",
+    "Operation",
+    "PlaceholderType",
+    "Session",
+    "Tensor",
+    "VariableType",
+    "assign",
     "concat",
     "conv2d",
-    "max_pool",
-    "reshape",
-    "matmul",
-    "multiply",
-    "squeeze",
-    "reduce_mean",
-    "square",
-    "gradients",
-    "GradientDescentOptimizer",
     "eye",
-    "hessians",
-    "zeros",
-    "assign",
-    "inv",
-    "Session",
     "global_variables_initializer",
-    "Tensor",
-    "Operation",
+    "gradients",
+    "hessians",
+    "inv",
+    "matmul",
+    "max_pool",
+    "multiply",
+    "placeholder",
+    "reduce_mean",
+    "reshape",
+    "split",
+    "square",
+    "squeeze",
+    "variable",
+    "zeros",
 ]
