536cac304fda180ed55c8495e8b0e89c9680828d
Andrey Tolstykh <xausssr@gmail.com>
2021-01-29 00:04:34 +0300

    @melchugin can you please allow me to merge,  I can not take it anymore!)

--- tests/test_iris_net.py
+++ tests/test_iris_net.py
@@ -3,16 +3,9 @@ import numpy as np
 from nnreslib.architecture import ArchitectureType
 from nnreslib.layers import FullyConnectedLayer, InputLayer
 from nnreslib.model import Model
-from nnreslib.utils.initialization import Initialization
 from nnreslib.utils.types import ActivationFunctions, Shape
 
-seed = np.random.RandomState(42)
-
-
-def he_normal(
-    input_shape: Shape, output_shape: Shape, parameter_shape: Shape, data_mean: float = 0.0, data_std: float = 0.0
-) -> np.ndarray:
-    return seed.uniform(size=parameter_shape.dimension)
+np.random.seed(42)
 
 
 def test_iris_net():
@@ -26,12 +19,11 @@ def test_iris_net():
 
     architecture: ArchitectureType = [
         InputLayer("input", Shape(4)),
-        FullyConnectedLayer("fc_1", neurons=5, initializer=Initialization(he_normal)),
-        FullyConnectedLayer("fc_2", neurons=6, initializer=Initialization(he_normal)),
+        FullyConnectedLayer("fc_1", neurons=5),
+        FullyConnectedLayer("fc_2", neurons=6),
         FullyConnectedLayer(
             "fc_3",
             neurons=3,
-            initializer=Initialization(he_normal),
             activation=ActivationFunctions.SOFT_MAX,
             is_out=True,
         ),
@@ -45,13 +37,13 @@ def test_iris_net():
         x_validation,
         y_validation,
         200,
-        0.034,
+        0.0005,
         step_into_epoch=10,
-        regularisation_factor_init=3.0,
+        regularisation_factor_init=5.0,
         regularisation_factor_decay=10.0,
         regularisation_factor_increase=10.0,
     )
 
-    assert epoch == 24
-    assert loss < 0.033
-    assert np.array_equal(model.predict(x_train)[0], np.array([0, 0, 1]))
+    assert epoch == 56
+    assert loss < 0.0005
+    assert np.array_equal(model.predict(x_train)[0], np.array([1, 0, 0]))
