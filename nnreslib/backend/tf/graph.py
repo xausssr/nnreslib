430303f866e273de0372a182468ff8407ce22c3b
Andrey Tolstykh <xausssr@gmail.com>
2021-07-25 23:38:56 +0300

    Optimization methods Add first-order methods:    * Adadelta    * Adagrad    * SGD    * Momentum    * RMSProp
    Add tests for first-order methods
    Add stability to Levenberg-Marquardt method
    
    Co-authored-by: Maxim Elchugin <melchugin@users.noreply.github.com>

--- nnreslib/backend/tf/graph.py
+++ nnreslib/backend/tf/graph.py
@@ -4,12 +4,17 @@ import tensorflow as tf
 
 from .. import DTYPE
 
+Adadelta = tf.compat.v1.train.AdadeltaOptimizer
+Adagrad = tf.compat.v1.train.AdagradOptimizer
 Adam = tf.compat.v1.train.AdamOptimizer
+DType = tf.as_dtype(DTYPE.value)
 Dataset = tf.compat.v1.data.Dataset
 GradientDescentOptimizer = tf.compat.v1.train.GradientDescentOptimizer
+Momentum = tf.compat.v1.train.MomentumOptimizer
 Operation = tf.Operation
 OutOfRangeError = tf.errors.OutOfRangeError
 PlaceholderType = tf.compat.v1.placeholder
+RMSProp = tf.compat.v1.train.RMSPropOptimizer
 Session = tf.compat.v1.Session
 Tensor = tf.Tensor
 TensorArray = partial(tf.TensorArray, dtype=DTYPE.value)
@@ -37,12 +42,17 @@ squeeze = tf.squeeze
 zeros = partial(tf.zeros, dtype=DTYPE.value)
 
 __all__ = [
+    "Adadelta",
+    "Adagrad",
     "Adam",
+    "DType",
     "Dataset",
     "GradientDescentOptimizer",
+    "Momentum",
     "Operation",
     "OutOfRangeError",
     "PlaceholderType",
+    "RMSProp",
     "Session",
     "Tensor",
     "TensorArray",
---
+++ nnreslib/graph/fit_graph/adagrad.py
@@ -0,0 +1,55 @@
+from typing import Any, Dict, Iterable, Tuple
+
+import numpy as np
+
+from .fit_graph import FitGraph, LossFunctionType
+from .. import ForwardGraph
+from ...architecture import Architecture
+from ...backend import graph as G
+from ...utils.types import LossFunctions
+
+
+@FitGraph.register_fitter()
+class Adagrad(FitGraph):
+    __slots__ = (
+        "optimizer",
+        "loss",
+        "train_loss",
+        "learning_rate",
+    )
+
+    def __init__(
+        self,
+        batch_size: int,
+        architecture: Architecture,
+        forward_graph: ForwardGraph,
+        loss: LossFunctionType = LossFunctions.MSE.value,
+        **kwargs: Dict[str, Any],
+    ) -> None:
+        super().__init__(batch_size, architecture, forward_graph, loss)
+        initial_accumulator_value = kwargs.get("initial_accumulator_value", 0.1)
+        self.learning_rate = kwargs.get("learning_rate", 0.001)
+        self.session = forward_graph.session
+        self.train_loss = loss(self.outputs, self.model_outputs)
+        self.optimizer = G.Adagrad(self.learning_rate, initial_accumulator_value).minimize(self.train_loss)
+        self.session.run(G.global_variables_initializer())
+
+    def _process_train_batch(
+        self, batch_x: np.ndarray, batch_y: Iterable[np.ndarray], **kwargs: Any
+    ) -> Tuple[float, Tuple[np.ndarray, ...], Tuple[Any, ...]]:
+        feed_dict = self._get_feed_dict(batch_x, batch_y)
+        current_loss = self.session.run(self.train_loss, feed_dict)
+        self.session.run(self.optimizer, feed_dict)
+        return (
+            current_loss,
+            self.session.run(self.forward_graph.outputs, feed_dict),
+            (),
+        )
+
+    def _process_valid_batch(
+        self, batch_x: np.ndarray, batch_y: Iterable[np.ndarray]
+    ) -> Tuple[float, Tuple[np.ndarray, ...]]:
+        feed_dict = self._get_feed_dict(batch_x, batch_y)
+        current_loss = self.session.run(self.train_loss, feed_dict)
+        output = self.session.run(self.forward_graph.outputs, feed_dict)
+        return current_loss, output
